053de2938de7efe7ca3f4caaa7a64c7e30199d37
Neal Granger <neal@metalab.co>
2016-04-22 10:36:33 -0700

    Cache root element between render iterations.

--- src/server/render.js
+++ src/server/render.js
@@ -8,41 +8,43 @@ import Root from 'component/root';
 
 import {API_URL} from 'config';
 
-function render(context, currentIteration = 1) {
-  const {relayEnvironment} = context;
+function render(element, context) {
+  function iterate(iteration = 1) {
+    return new Promise((resolve) => {
+      // Render the element tree to an HTML string. renderToString does not
+      // execute the full component lifecycle, but does call componentWillMount.
+      // This gives any Relay containers the opportunity to run queries, which
+      // are stored in the context.
+      const markup = renderToString(element);
 
-  return new Promise((resolve) => {
-    // Render the component tree to an HTML string. renderToString does not
-    // execute the full component lifecycle, but does call componentWillMount.
-    // This gives any Relay containers the opportunity to run queries, which are
-    // stored in the context.
-    const markup = renderToString(<Root relayEnvironment={relayEnvironment} />);
+      // Retrieve and clear the stored promises from the context. This returns
+      // an array of js promises representing each query that was run during the
+      // current render iteration. Each promise will resolve in turn as each
+      // query result is returned from the GraphQL endpoint.
+      const promises = context.flushPromises();
 
-    // Retrieve and clear the stored promises from the context. This returns an
-    // array of js promises representing each query that was run during the
-    // current render iteration. Each promise will resolve in turn as each query
-    // result is returned from the GraphQL endpoint.
-    const promises = context.flushPromises();
+      if (iteration === 4) {
+        // We have reached the maximum number of recursive render iterations.
+        // Avoid a potentially endless loop and return the most recent render
+        // result to the client.
+        return resolve(markup);
+      }
 
-    if (currentIteration === 4) {
-      // We have reached the maximum number of recursive render iterations.
-      // Avoid a potentially endless loop and return the most recent render
-      // result to the client.
-      return resolve(markup);
-    }
+      if (!promises.length) {
+        // The current render iteration did not run any queries, this means the
+        // rendered markup is ready to be delivered to the client.
+        return resolve(markup);
+      }
 
-    if (!promises.length) {
-      // The current render iteration did not run any queries, this means the
-      // rendered markup is ready to be delivered to the client.
-      return resolve(markup);
-    }
-
-    // Wait for all of the relay queries to resolve then start another render
-    // iteration.
-    return Promise.all(promises).then(() => {
-      resolve(render(context, currentIteration + 1));
+      // Wait for all of the relay queries to resolve then start another render
+      // iteration.
+      return Promise.all(promises).then(() => {
+        resolve(iterate(iteration + 1));
+      });
     });
-  });
+  }
+
+  return iterate();
 }
 
 export default function(app) {
@@ -59,24 +61,25 @@ export default function(app) {
       // fail on the server. Here we need to specify an absolute URL to the
       // GraphQL endpoint for the server's sake.
       const context = createContext({apiURL: API_URL});
+      const {relayEnvironment} = context;
 
       // Call render providing the context instance. render returns a promise
       // which resolves with the rendered HTML result after all of the queries
       // for any Relay containers are done.
-      render(context).then((markup) => {
-        // Stringify the saved query results to JSON using htmlescape.
-        const state = escape(context.getRelayState());
+      render(<Root relayEnvironment={relayEnvironment} />, context)
+        .then((markup) => {
+          // Stringify the saved query results to JSON using htmlescape.
+          const state = escape(context.getRelayState());
 
-        // Use React to generate the HTML page layout.
-        const page = renderToStaticMarkup(
-          <Page markup={markup} state={state} />
-        );
-        const body = `<!DOCTYPE html>${page}`;
+          // Use React to generate the HTML page layout.
+          const body = `<!DOCTYPE html>${
+            renderToStaticMarkup(<Page markup={markup} state={state} />)
+          }`;
 
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html; charset=utf-8');
-        res.end(body);
-      });
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/html; charset=utf-8');
+          res.end(body);
+        });
     },
   };
 }
